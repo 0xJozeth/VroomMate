9c7f65d4a71a8ee6f0af2ccee062c254799c126f
Glen <0xJozeth@gmail.com>
2023-07-26 14:36:20 -0400

    add API fetch functionality |update car catalogue ui | wip

--- car_showcase/components/CarCard.tsx
+++ car_showcase/components/CarCard.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { CarProps } from '@/types';
 import { CustomButton } from '.';
+import { calculateCarRent } from '@/utils';
 
 interface carCardProps {
 	car: CarProps;
@@ -11,13 +12,48 @@ interface carCardProps {
 
 const CarCard = ({ car }: carCardProps) => {
 	const { city_mpg, year, make, model, transmission, drive } = car;
+	const carRent = calculateCarRent(city_mpg, year);
 
 	return (
 		<div className='car-card group'>
 			<div className='car-card__content'>
-				<h2>
+				<h2 className='car-card__content-title'>
 					{make} {model}
 				</h2>
+			</div>
+
+			<p className='flex mt-6 text-[32px] font-extrabold'>
+				<span className='self-start text-[14px] font-semibold'>$</span>
+				{carRent}
+				<span className='self-end text-[14px] font-medium'>/day</span>
+			</p>
+
+			<div className='relative w-full h-40 my-3 object-contain'>
+				<Image
+					src='/hero.png'
+					// width={50}
+					// height={50}
+					alt='car model'
+					fill
+					priority
+					className='object-contain'
+				/>{' '}
+			</div>
+
+			<div className='relative flex w-full mt-2'>
+				<div className='flex group-hover:invible w-full justify-between text-gray'>
+					<div className='flex flex-col justify-center items-center gap-2'>
+						<Image
+							src='/steering-wheel.svg'
+							width={20}
+							height={20}
+							alt='steering wheel'
+						/>
+						<p className='text-[14px]'>
+							{transmission === 'a' ? 'Automatic' : 'Manual'}
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
